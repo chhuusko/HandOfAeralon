e762d1d265ddba0b93b27295b55c16aa17117921
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-06 18:27:04 +0100

    Update AbilityHandler.cs

--- Proj/Assets/Scripts/Abilities/AbilityHandler.cs
+++ Proj/Assets/Scripts/Abilities/AbilityHandler.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class AbilityHandler : MonoBehaviour
 {
     [SerializeField] private List<Ability> abilities;
-    
+    [SerializeField] private CombatGrid combatGrid;
+
     private void UseAbility(Ability a)
     {
 
