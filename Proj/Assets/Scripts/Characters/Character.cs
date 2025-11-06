62b1ae1436382f9e6ce6f967b4927cdd110899eb
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-11-06 17:17:02 +0100

    Basic character functionality
    - Added basic movement, needed members and class enum

--- Proj/Assets/Scripts/Characters/Character.cs
+++ Proj/Assets/Scripts/Characters/Character.cs
@@ -1,16 +1,61 @@
+using System;
 using UnityEngine;
 
+public enum CharacterClass { Rogue, Wizard }
+
+[RequireComponent(typeof(Rigidbody))]
 public class Character : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private const float MOVE_SPEED = 5f;
+    private int _healthPoints;
+    private int _speed;
+    // TODO: Traits.
+    private Vector3 _movePosition;
+    private bool _bShouldMove;
+    private Rigidbody _rigidbody;
+    private CharacterClass _characterClass;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        if (_bShouldMove)
+        {
+            Move();
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _healthPoints -= damage;
+    }
+
+    public void Heal(int healAmount)
+    {
+        _healthPoints += healAmount;
+    }
+
+    public void SetMoveTarget(CombatGridTile target)
+    {
+        // Start moving.
+        _movePosition = target.GetTilePosition();
+        _bShouldMove = true;
         
+        // Look toward goal.
+        Vector3 lookDirection = _movePosition - transform.position;
+        transform.rotation = Quaternion.LookRotation(lookDirection);
+    }
+
+    private void Move()
+    {
+        // Moves toward goal.
+        Vector3 targetVelocity = transform.forward * MOVE_SPEED;
+        Vector3 currentVelocity = _rigidbody.linearVelocity;
+        Vector3 velocityChange = targetVelocity - currentVelocity;
+            
+        _rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
     }
 }
