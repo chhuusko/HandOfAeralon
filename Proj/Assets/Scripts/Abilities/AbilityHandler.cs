016c2f2cec3b1044d989001025d48160c3b37943
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-10 09:35:52 +0100

    AbilityHandler Update

--- Proj/Assets/Scripts/Abilities/AbilityHandler.cs
+++ Proj/Assets/Scripts/Abilities/AbilityHandler.cs
@@ -3,11 +3,35 @@ using UnityEngine;
 
 public class AbilityHandler : MonoBehaviour
 {
-    [SerializeField] private List<Ability> abilities;
-    [SerializeField] private CombatGrid combatGrid;
+    //[SerializeField] private List<Ability> _abilities;
+    //[SerializeField] private CombatGrid _combatGrid;
 
-    private void UseAbility(Ability a)
-    {
+    //List<CombatGridTile> _availableAbilityTargets;
 
-    }
+    ////characterCaster referens
+
+    //private void Start()
+    //{
+    //    // Character characterCaster = GetComponent<Character>
+    //}
+    //private void UseAbility(Ability ability /*, targetTile */)
+    //{
+    //    if (!CanCastAbility())
+    //    {
+    //        Debug.Log("Tried casting ability on inaccaptable target.");
+    //        return;
+    //    }
+
+    //    ability.RunAbility(characterCaster.currentTile, targetTile);
+    //}
+    
+    //private bool CanCastAbility(/* targetTile */)
+    //{
+    //    return _availableAbilityTargets.Contains(targetTile);
+    //}
+    
+    //private void CheckAbilityTargets(Ability ability)
+    //{
+    //    //_availableAbilityTargets = ability.targetingPattern.GetVaildTiles(characterCaster.currentTile)
+    //} 
 }
