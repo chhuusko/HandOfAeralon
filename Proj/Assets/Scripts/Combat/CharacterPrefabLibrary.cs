3abbe70ee02a963714139c57f1532441521bceef
xWormx <42252397+xWormx@users.noreply.github.com>
2025-11-11 09:43:40 +0100

    EDIT - CharacterPrefabLib now store lists of modelprefabs

--- Proj/Assets/Scripts/Combat/CharacterPrefabLibrary.cs
+++ Proj/Assets/Scripts/Combat/CharacterPrefabLibrary.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class CharacterPrefabEntry
 {
-    public CharacterClass classType;
-    public GameObject prefab;
+    public CharacterClass _classType;
+    public List<GameObject> _prefabList;
 }
 
 [CreateAssetMenu(fileName = "CharacterPrefabLibrary", menuName = "Scriptable Objects/Character Prefab Library")]
@@ -14,19 +14,26 @@ public class CharacterPrefabLibrary : ScriptableObject
 {
     public List<CharacterPrefabEntry> _characterPrefabs;
 
-    private Dictionary<CharacterClass, GameObject> _dictionary;
+    private Dictionary<CharacterClass, List<GameObject>> _dictionary;
 
     public GameObject GetPrefab(CharacterClass classType)
     {
         if (_dictionary == null)
         {
-            _dictionary = new Dictionary<CharacterClass, GameObject>();
-            foreach (var entry in _characterPrefabs)
+            _dictionary = new Dictionary<CharacterClass, List<GameObject>>();
+            foreach (CharacterPrefabEntry entry in _characterPrefabs)
             {
-                _dictionary[entry.classType] = entry.prefab;
+                List<GameObject> list = _dictionary[entry._classType];
+                list.Add(entry._prefabList[0]);
             }
         }
 
-        return _dictionary[classType];
+        // TODO (Calle): Wait to loop over the list untill we have a gender! :D
+        //foreach(GameObject character in _dictionary[classType])
+        //{
+        //    if(character.GetComponent<Character>().GetGender() == _dictionary[])
+        //}
+        List<GameObject> characterListOfClass = _dictionary[classType];
+        return characterListOfClass[0];
     }
 }
