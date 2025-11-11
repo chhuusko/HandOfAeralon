f41468e942bb4371ee71aa9d5a4c91754f95ff4c
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-11-11 14:46:49 +0100

    GetMana and ChangeMana

--- Proj/Assets/Scripts/Card/CardHandManager.cs
+++ Proj/Assets/Scripts/Card/CardHandManager.cs
@@ -13,14 +13,17 @@ public class CardHandManager : MonoBehaviour
     [SerializeField] private List<CardContainer> _cardsInHand;
     [SerializeField] private int _maxHand = 3;
     [SerializeField] private CardList _cardList;
+    private int _maxMana = 5;
     private int _mana = 0;
 
+    public static Action<int> onManaChange;
+    public static CardHandManager GetInstance() {return _instance;}
+    public void ManaChanged(){ onManaChange?.Invoke(_mana); }
     private void Awake()
     {
         _instance = this;
         drawHand();
     }
-    public static CardHandManager GetInstance() {  return _instance; }
     public void drawHand()
     {
         _cardsInHand.RemoveAll(o => o == null);
@@ -40,7 +43,6 @@ public class CardHandManager : MonoBehaviour
 
     public void AddSpaceing()
     {
-        
         for (int i = 0; i < _cardsInHand.Count; i++)
         {
             Vector3 position = transform.position + new Vector3(-(150f * (_cardsInHand.Count - 1)) / 2f, 0, 0) + new Vector3(i * 150f, 0, 0);
@@ -61,19 +63,21 @@ public class CardHandManager : MonoBehaviour
             _cardsInHand.Clear();
             drawHand();
         }
-        
-        
     }
     public void RemoveCard(CardContainer cardContainer)
     {
-        
         _cardsInHand.Remove(cardContainer);
         Destroy(cardContainer.gameObject);
         drawHand();
     }
     public void ChangeMana(int change)
     {
-
+        _mana += change;
+        ManaChanged();
+    }
+    public int GetMaxMana()
+    {
+        return _maxMana;
     }
     public void SetUIActive(bool isActive)
     {
