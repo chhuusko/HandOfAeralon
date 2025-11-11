055fd24bb67c9151dffb0ac860d550b258aeb603
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-11 13:21:42 +0100

    temp ability

---
+++ Proj/Assets/Scripts/Abilities/Specific Abilites/FireBolt_SingleTarget.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FireBolt_SingleTarget : SingleTargetAbility
+{
+    [Header("- Ability Specific values -")]
+    [SerializeField] private float _damage;
+
+    protected override void ApplyEffectOnTile(CombatGridTile tileToEffect)
+    {
+
+    }
+}
