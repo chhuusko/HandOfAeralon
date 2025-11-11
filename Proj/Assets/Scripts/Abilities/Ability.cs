00cffbc7d7fca7dbc977cca6cdec932ece8ce863
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-11 10:42:43 +0100

    Selector update, ability getters, ability handler update

--- Proj/Assets/Scripts/Abilities/Ability.cs
+++ Proj/Assets/Scripts/Abilities/Ability.cs
@@ -10,6 +10,8 @@ public abstract class Ability : ScriptableObject
 
     public abstract void RunAbility(CombatGridTile casterTile, CombatGridTile targetTile);
 
+    public string GetAbilityName => _abilityName;
+    public Sprite GetIcon => _icon;
     public float GetRange => _range;
 
     
