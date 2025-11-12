742a42d46d9b299dd6e4c70a8ac4de6f85e85739
Carlroos00 <carl.roos@hotmail.com>
2025-11-12 11:53:10 +0100

    Busat i Joels scen

--- Proj/Assets/Scripts/Characters/Character.cs
+++ Proj/Assets/Scripts/Characters/Character.cs
@@ -181,7 +181,7 @@ public class Character : MonoBehaviour
 
         // Agent is considered moving if it has velocity and hasn't reached destination
         return _navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance
-               || _navMeshAgent.velocity.sqrMagnitude > 0.01f;
+               || _navMeshAgent.velocity.sqrMagnitude > 0.1f;
     }
 
     /// <summary>
