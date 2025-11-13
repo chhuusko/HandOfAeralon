02e8a29e3a93b5126acff93911f40b7ea623d65f
xWormx <42252397+xWormx@users.noreply.github.com>
2025-11-13 11:38:28 +0100

    EDIT - Moved DebugLog klass to DEBUGSettings

--- Proj/Assets/Scripts/DEBUG/DEBUGSettings.cs
+++ Proj/Assets/Scripts/DEBUG/DEBUGSettings.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "DEBUGSettings", menuName = "Scriptable Objects/DEBUGSettings")]
@@ -9,3 +10,49 @@ public class DEBUGSettings : ScriptableObject
     public bool AlexLog;
     public bool JoppaLog;
 }
+
+
+public static class DebugLog
+{
+    private static DEBUGSettings _settings;
+
+    private static void LoadSettings()
+    {
+        if (_settings == null)
+            _settings = AssetDatabase.LoadAssetAtPath<DEBUGSettings>("Assets/ScriptableObject/DEBUG/DEBUGSettings.asset");
+    }
+    public static void CJLog(string message)
+    {
+        LoadSettings();
+        if (_settings && _settings.CJLog)
+            Debug.Log("CJ Log: " + message);
+    }
+    public static void MGLog(string message)
+    {
+        LoadSettings();
+        if (_settings && _settings.MGLog)
+
+            Debug.Log("MG Log: " + message);
+    }
+    public static void JLWLog(string message)
+    {
+        LoadSettings();
+        if (_settings && _settings.JLWLog)
+
+            Debug.Log("JLW Log: " + message);
+    }
+    public static void AlexLog(string message)
+    {
+        LoadSettings();
+        if (_settings && _settings.AlexLog)
+
+            Debug.Log("Alex Log: " + message);
+    }
+    public static void JoppaLog(string message)
+    {
+        LoadSettings();
+        if (_settings && _settings.JoppaLog)
+
+            Debug.Log("Joppa Log: " + message);
+    }
+};
