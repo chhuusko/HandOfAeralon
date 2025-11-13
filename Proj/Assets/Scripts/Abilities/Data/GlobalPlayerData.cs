87e53e2d8b1803e67758a0d7e839ef49b06c989b
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-11-13 10:36:06 +0100

    GlobalPlayerData

---
+++ Proj/Assets/Scripts/Abilities/Data/GlobalPlayerData.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalPlayerData : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private static GlobalPlayerData _instance;
+    public List<Character> heroList;
+    public List<Card> cardsList;
+    private void Awake()
+    {
+        _instance = this;
+    }
+    public GlobalPlayerData GetInstance()
+    {
+        return _instance;
+    }
+    public List<Character> GetHeroList()
+    {
+        return heroList;
+    }
+    public List<Card> GetCardList()
+    {
+        return cardsList;
+    }
+    public void SetHeroList(List<Character> newHeroList)
+    {
+        heroList = newHeroList;
+    }
+    public void SetCardList(List<Character> newHeroList)
+    {
+        heroList = newHeroList;
+    }
+}
