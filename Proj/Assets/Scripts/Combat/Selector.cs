4cc06d9e6295164e36e132fa120306084c7258cd
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-13 14:13:03 +0100

    Update Selector.cs

--- Proj/Assets/Scripts/Combat/Selector.cs
+++ Proj/Assets/Scripts/Combat/Selector.cs
@@ -5,6 +5,14 @@ using UnityEngine.EventSystems;
 public class Selector : MonoBehaviour
 {
     public static Selector _instance {  get; private set; }
+    public enum SelectorState
+    {
+        NonActive,
+        PlacingCharacters,
+        Idle,  
+        CharacterSelected,
+        ActionTypeSelected,
+    }
 
     private void Awake()
     {
@@ -18,14 +26,6 @@ public class Selector : MonoBehaviour
         }
     }
 
-    public enum SelectorState
-    {
-        NonActive,
-        PlacingCharacters,
-        Idle,  
-        CharacterSelected,
-        ActionTypeSelected,
-    }
     public enum CharacterActionType
     {
         Null,
