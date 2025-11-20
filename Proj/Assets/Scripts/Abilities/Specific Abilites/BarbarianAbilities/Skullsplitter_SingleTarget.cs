f9f5e118986f72b6569983065ba0f60e4e23c86e
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-20 11:48:44 +0100

    Ability folders and Skullsplitter

---
+++ Proj/Assets/Scripts/Abilities/Specific Abilites/BarbarianAbilities/Skullsplitter_SingleTarget.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Skullsplitter_Ability", menuName = "Scriptable Objects/Abilities/Range Calculations/Non-Blocking")]
+public class Skullsplitter_Ability : SingleTargetAbility
+{
+    [Header("- Ability Specific values -")]
+    [SerializeField] private int _baseDamage;
+    [SerializeField] private float damageMultiplier = 1.6f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void ApplyEffectOnTile(CombatGridTile tileToEffect)
+    {
+        if (tileToEffect == null) return;
+
+        Character affectedCharacter = tileToEffect.GetOccupantCharacter();
+        if (affectedCharacter == null) return;
+
+        // Calculate increasedDamage;
+        int increasedDamage = (int) (_baseDamage * damageMultiplier);
+
+        // Decide which damage to use.
+        int calculatedDamge = affectedCharacter.GetHealthPoints() < (0.5 * affectedCharacter.GetBaseHealthPoints()) ? increasedDamage : _baseDamage;
+ 
+
+        affectedCharacter.TakeDamage(_baseDamage);
+    }
+
+}
