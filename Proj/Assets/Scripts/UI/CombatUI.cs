cbab63503ec6d3a9cd9a92f68cf1fa562dd393d9
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-11-20 10:33:26 +0100

    Added field for turn order panel
    - Not yet implemented

--- Proj/Assets/Scripts/UI/CombatUI.cs
+++ Proj/Assets/Scripts/UI/CombatUI.cs
@@ -15,10 +15,13 @@ public class CombatUI : MonoBehaviour
     [SerializeField] private Image _abilityPanel;
     [SerializeField] private Image _characterPortraitPanel;
     [SerializeField] private Image _activeCharacterPortrait;
+    [SerializeField] private Image _turnOrderPanel;
+    
     [SerializeField] private Button _startCombatButton;
     [SerializeField] private Button _endTurnButton;
     [SerializeField] private Button _abilityButtonPrefab;
     [SerializeField] private Button _characterPortraitButtonPrefab;
+    
     [SerializeField] private GameObject _hand;
     [SerializeField] private TextMeshProUGUI _mana;
 
