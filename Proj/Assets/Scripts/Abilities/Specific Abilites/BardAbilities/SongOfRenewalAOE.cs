144adeefd752d1db8c59c51112e1b49a67607226
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-21 09:35:48 +0100

    Song Of Renewal

---
+++ Proj/Assets/Scripts/Abilities/Specific Abilites/BardAbilities/SongOfRenewalAOE.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongOfRenewalAOE : AOEAbility
+{
+    [Header("- Ability Specific values -")]
+    [SerializeField] private float _maxHealthHealMain = 0.25f;
+    [SerializeField] private float _maxHealthHealArea = 0.1f;
+
+    public override void RunAbility(CombatGridTile casterTile, CombatGridTile targetTile)
+    {
+        // Calculate all tiles around with in radius and apply effect to all of them.
+
+        List<CombatGridTile> tilesToEffect = _pattern.CalculateTilesToEffect(targetTile);
+
+        foreach (CombatGridTile tile in tilesToEffect)
+        {
+            if (tile == null) continue;
+
+            if (tile == targetTile)
+            {
+                ApplyEffectOnMainTile(casterTile, tile);
+                continue;
+            }
+            ApplyEffectOnTile(casterTile, tile);
+        }
+    }
+
+    protected override void ApplyEffectOnTile(CombatGridTile casterTile, CombatGridTile tileToEffect)
+    {
+        if (tileToEffect == null) return;
+
+        Character affectedCharacter = tileToEffect.GetOccupantCharacter();
+        if (affectedCharacter == null) return;
+        Character castingCharacter = casterTile.GetOccupantCharacter();
+        if (castingCharacter == null) return;
+
+        affectedCharacter.Heal(CalculateHealAmount(castingCharacter, affectedCharacter, false));
+    }
+
+    private void ApplyEffectOnMainTile(CombatGridTile casterTile, CombatGridTile tileToEffect)
+    {
+        if (tileToEffect == null) return;
+
+        Character affectedCharacter = tileToEffect.GetOccupantCharacter();
+        if (affectedCharacter == null) return;
+        Character castingCharacter = casterTile.GetOccupantCharacter();
+        if (castingCharacter == null) return;
+
+        affectedCharacter.Heal(CalculateHealAmount(castingCharacter, affectedCharacter, true));
+    }
+
+    private int CalculateHealAmount(Character castingCharacter, Character affectedCharacter, bool bIsMainTarget)
+    {
+        // 1. Character health
+        // 2. Ability Heal
+        // 3. Your Traits
+        // 4. Your Buffs/ Debuffs(Kan även appliceras av kort)
+        // 5. Eventuella Ability Global Modifiers(Ex.Arena modifiers)
+        // 6. EnemyTraits
+        // 7. Enemy Buffs / Debuffs
+
+        //1.
+        //2.
+        float healMultiplier = bIsMainTarget ? _maxHealthHealMain : _maxHealthHealArea;
+        int healAmount = (int) (affectedCharacter.GetMaxHealth() * healMultiplier);
+
+        if (bIsMainTarget)
+        {
+            if (affectedCharacter.GetCurrentHealth() < (int) (affectedCharacter.GetMaxHealth() * 0.5f))
+            {
+                // CardManager._instance.DrawACard();
+            }
+        }
+
+        //3-5.
+        // healAmount = castingCharacter.GetStatusEffectManager().ModifyOutgoingHeal(healAmount, this);
+
+        return healAmount; 
+        // 6-7 Gets applied withing affected character StatusEffectManager: ModifyOutgoingHeal(healAmount, this);
+    }
+
+    protected override void InitiateParticles(CombatGridTile casterTile, CombatGridTile targetTile)
+    {
+        // Spawn and direct VFX to target location.
+    }
+}
