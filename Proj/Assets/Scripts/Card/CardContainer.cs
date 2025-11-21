e96f8fe6948e9b45fbe8a8d02c4c67070fb6788f
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-11-21 15:53:48 +0100

    Chaotic Burst

--- Proj/Assets/Scripts/Card/CardContainer.cs
+++ Proj/Assets/Scripts/Card/CardContainer.cs
@@ -67,9 +67,6 @@ public class CardContainer : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
         {
             Destroy(Instantiate(_particleDrop, _spawnedParticle.transform.position, Quaternion.identity), 2f);
             Destroy(_spawnedParticle);
-
-            Character character = Selector._instance.GetTileUnderMouse().GetOccupant().GetComponent<Character>();
-            character.TakeDamage(10);
             _containedCard.PlayCard();
             CardHandManager.GetInstance().RemoveCard(this);   
         }
@@ -133,5 +130,12 @@ public class CardContainer : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
     public Card GetCard()
     {
         return _containedCard;
+    }
+=======
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!CanAfford() && _containedCard.type == Card.CardType.Instant) return;
+        _containedCard.PlayCard();
     }
 }
--- Proj/Assets/Scripts/Card/Specific Cards/ChaoticBurst.cs
+++ Proj/Assets/Scripts/Card/Specific Cards/ChaoticBurst.cs
@@ -4,6 +4,22 @@ public class ChaoticBurst : Card
 {
     public override void PlayCard()
     {
+<<<<<<< Updated upstream
         //när den spelas
+=======
+
+        int damage = 15;
+        List<Character> character = CombatGrid._instance.GetAllCharacterScripts();
+        Debug.Log(character.Count);
+        if (character.Count <= 0) return;
+        while (damage > 0) 
+        {
+
+            int damageInstance = Random.Range(1, damage + 1);
+            character[Random.Range(0, character.Count)].TakeDamage(damageInstance);
+            damage -= damageInstance;
+            
+        }
+>>>>>>> Stashed changes
     }
 }
