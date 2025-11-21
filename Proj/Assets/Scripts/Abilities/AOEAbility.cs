eba3945293be09f5d9d18e77866a2405a2e61e51
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-21 16:43:42 +0100

    Update AOEAbility.cs

--- Proj/Assets/Scripts/Abilities/AOEAbility.cs
+++ Proj/Assets/Scripts/Abilities/AOEAbility.cs
@@ -1,16 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AOEAbility : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public abstract class AOEAbility : Ability
     {
-        
+        [Header("- Type Specific values - ")]
+        [SerializeField] protected AOEPattern _pattern;
+        public override void RunAbility(CombatGridTile casterTile, CombatGridTile targetTile)
+        {
+            // Calculate all tiles around with in radius and apply effect to all of them.
+            List<CombatGridTile> tilesToEffect = _pattern.CalculateTilesToEffect(targetTile);
+
+            foreach (CombatGridTile tile in tilesToEffect)
+            {
+                if (tile != null)
+                {
+                    ApplyEffectOnTile(casterTile, tile);
+                }
+            }
+        }
+        public override List<CombatGridTile> GetTilesToEffect(CombatGridTile tile)
+        {
+            return _pattern.CalculateTilesToEffect(tile);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
