0ad8ea5f77a31f4ce482faa31c70839e6a8e81ad
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-24 15:57:29 +0100

    Sandfang

---
+++ Proj/Assets/Scripts/Abilities/Specific Abilites/RogueAbilities/SandfangStrike_SingleTarget.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SandfangStrike_Ability", menuName = "Scriptable Objects/Abilities/Bard/Sandfang Strike")]
+public class SandfangStrike_SingleTarget : SingleTargetAbility
+{
+    [Header("- Ability Specific values -")]
+    [SerializeField] private float damageMultiplier = 1.6f;
+
+
+    protected override void ApplyEffectOnTile(CombatGridTile casterTile, CombatGridTile tileToEffect)
+    {
+        if (tileToEffect == null) return;
+
+        Character affectedCharacter = tileToEffect.GetOccupantCharacter();
+        if (affectedCharacter == null) return;
+        Character castingCharacter = casterTile.GetOccupantCharacter();
+        if (castingCharacter == null) return;
+
+        affectedCharacter.TakeDamage(CalculateDamage(castingCharacter, affectedCharacter));
+    }
+
+    private int CalculateDamage(Character castingCharacter, Character affectedCharacter)
+    {
+        // 1. Your Base Damage(Kan �kas med traits och eller kort.)
+        // 2. Ability damage.
+        // 3. Your Traits
+        // 4. Your Buffs/ Debuffs(Kan �ven appliceras av kort)
+        // 5. Eventuella Ability Global Modifiers(Ex.Arena modifiers)
+        // 6. EnemyTraits
+        // 7. Enemy Buffs / Debuffs
+
+        //1.
+        int damage = castingCharacter.GetBaseDamage();
+
+        //2.
+       
+
+        //3-5.
+        // damage = castingCharacter.GetStatusEffectManager().ModifyOutgoingDamage(damage, this);
+        return damage;
+
+        // 6-7 Gets applied withing affected character StatusEffectManager: ModifyOutgoingDamage(damage, this);
+    }
+
+    protected override void InitiateParticles(CombatGridTile casterTile, CombatGridTile targetTile)
+    {
+        // Spawn and direct VFX to target location.
+    }
+}
