07a2b19c16221cc5545adff184bd33fe2ab83349
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-11-25 10:26:15 +0100

    Reworked status effect base class
    - Changed damage to float.
    - Moved buff data to subclasses.
    - Added duration in base class as constructor parameter.

--- Proj/Assets/Scripts/Characters/StatusEffects/Buffs/Empowered.cs
+++ Proj/Assets/Scripts/Characters/StatusEffects/Buffs/Empowered.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class Empowered : StatusEffect
 {
+    private static StatusEffectData _data;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Empowered(Character character, StatusEffectData buffData) : base(character, buffData)
+    public Empowered(Character character, int duration) : base(character, duration)
     {
-        
     }
-    
-    
+
+    public override void SetData(StatusEffectData data)
+    {
+        _data = data;
+    }
+
+    public override void ModifyOutgoingDamage(ref float damage)
+    {
+        damage *= 1.5f;
+    }
 }
