5a874ab1cede423ed8b5668d9cdbd1f1e9d6b65d
MRbubbelgum <malte.gundhus@gmail.com>
2025-11-27 08:57:45 +0100

    Earthquake progress & SongOfRenewal update

--- Proj/Assets/Scripts/Abilities/Specific Abilites/BarbarianAbilities/Earthquake_AOE.cs
+++ Proj/Assets/Scripts/Abilities/Specific Abilites/BarbarianAbilities/Earthquake_AOE.cs
@@ -10,26 +10,33 @@ public class Earthquake_AOE : AOEAbility
 
     [SerializeField] private int _charactersSlowedToGetMana = 2;
     [SerializeField] private int _manaGain = 1;
-    protected override void ApplyEffectOnTile(CombatGridTile casterTile, CombatGridTile targetTile)
+    protected override void ApplyEffectOnTile(CombatGridTile casterTile, CombatGridTile tileToEffect)
     {
-        
+        if (tileToEffect == null) return;
+
+        Character affectedCharacter = tileToEffect.GetOccupantCharacter();
+        if (affectedCharacter == null) return;
+        Character castingCharacter = casterTile.GetOccupantCharacter();
+        if (castingCharacter == null) return;
+
+        int damage = CalculateDamage(castingCharacter, affectedCharacter);
+        affectedCharacter.TakeDamage(damage);
+        AbilityExecutionData.Create(this, castingCharacter, affectedCharacter, tileToEffect, damage, 0);
+
+
+        int randomNum = Random.Range(1, 11);
+        if ((randomNum / 10) > _slowCharacterHitChance)
+        {
+
+        }
     }
 
     private int CalculateDamage(Character castingCharacter, Character affectedCharacter)
     {
-   
-
-        //1.
-        int damage = castingCharacter.GetBaseDamage();
-
-        //2.
-        damage = affectedCharacter.GetCurrentHealth() < (0.5 * affectedCharacter.GetMaxHealth()) ? (int)(damage * _damageMultiplier) : damage;
-
+        int damage = (int)(castingCharacter.GetBaseDamage() * _damageMultiplier);
         damage = (int)castingCharacter.GetStatusEffectManager().ModifyOutgoingDamage(damage, this);
         damage = (int)affectedCharacter.GetStatusEffectManager().ModifyIncomingDamage(damage, this);
-
         return damage;
-
     }
 
     protected override void InitiateParticles(CombatGridTile casterTile, CombatGridTile targetTile)
