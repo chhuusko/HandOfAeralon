dbabb6c57d0b8406f3c56963aa4dc517cea80ef4
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-11-27 11:53:45 +0100

    EndSceneChanges

--- Proj/Assets/EndSceneHandler.cs
+++ Proj/Assets/EndSceneHandler.cs
@@ -1,16 +1,26 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndSceneHandler : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] Image[] ExpBar;
+    [SerializeField] int ExpBarSize = 20;
+    int expGain;
+    private void Start()
     {
-        
+        GameData gameData = GlobalGameManager.GetInstance().GetGameData();
+        expGain = 50; 
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator expBarChange()
     {
-        
+        for (int i = 0;  i < expGain; i++)
+        {
+            foreach(Image item in ExpBar)
+            {
+                yield return item; 
+            }
+        }
     }
 }
