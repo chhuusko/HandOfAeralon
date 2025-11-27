a63ab88b0eb4e6fff62c6def7fae9136f9a0227f
xWormx <42252397+xWormx@users.noreply.github.com>
2025-11-27 15:17:26 +0100

    FIX - Cursor Loading

--- Proj/Assets/Scripts/RuntimeInit/RuntimeInit.cs
+++ Proj/Assets/Scripts/RuntimeInit/RuntimeInit.cs
@@ -5,16 +5,17 @@ public class RuntimeInit
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     static void OnAppStart()
     {
-        CursorDatabase cursorDatabase = Resources.Load<CursorDatabase>("CursorDatabase");
+        CursorDatabase cursorDatabase = Resources.Load<CursorDatabase>("ScriptableObjects/CursorDatabase");
         
         if(cursorDatabase != null )
         {
-
-            Texture2D cursor = Resources.Load<Texture2D>("Cursor/MyCursor");
-            Vector2 hotspot = Vector2.zero;
-
-            Cursor.SetCursor(cursor, hotspot, CursorMode.Auto);
+            Texture2D cursor = cursorDatabase.defaultCursor;
+            Cursor.SetCursor(cursorDatabase.defaultCursor, Vector2.zero, CursorMode.Auto);
         }
-        
+        else
+        {
+            DebugLog.CJLog("CursorDatabase not found in Resources folder.");
+        }
+
     }
 }
