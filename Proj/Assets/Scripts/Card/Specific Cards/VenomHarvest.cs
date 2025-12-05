6d949e19fea0c4d8de84940db6a8c004a8d21271
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-12-05 15:03:07 +0100

    VenomHarvest

---
+++ Proj/Assets/Scripts/Card/Specific Cards/VenomHarvest.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Venom Harvest", menuName = "Item/Card Data/Venom Harvest", order = 1)]
+public class VenomHarvest : Card
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void PlayCard()
+    {
+        Character character = Selector._instance.GetTileUnderMouse().GetOccupantCharacter();
+
+        if (character != null)
+        {
+            int totalDamage = 0;
+            if (character.GetStatusEffectManager().ContainsStatusEffect<Poison>())
+            {
+                StatusEffect poison = character.GetStatusEffectManager().GetStatusEffect<Poison>();
+                
+                for (int i = poison.Duration; i > 0; i--)
+                {
+                    totalDamage += i; 
+                }
+            }
+            character.TakeDamage(totalDamage);
+        }
+    }
+}
