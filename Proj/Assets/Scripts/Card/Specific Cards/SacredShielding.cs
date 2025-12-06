ad7261e199a16fc1a9f85e3af41d24009d563d6a
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-12-06 15:54:43 +0100

    sacredShieldFix

--- Proj/Assets/Scripts/Card/Specific Cards/SacredShielding.cs
+++ Proj/Assets/Scripts/Card/Specific Cards/SacredShielding.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "Sacred Shielding", menuName = "Item/Card Data/Sacred Shielding", order = 1)]
 public class SacredShielding : Card
@@ -9,6 +10,14 @@ public class SacredShielding : Card
 
         if (character != null)
         {
+            List<StatusEffect> statuses = new List<StatusEffect>(character.GetStatusEffectManager().GetAllEffects());
+            foreach (StatusEffect status in statuses)
+            {
+                if (status.Data.Type == StatusEffectType.Debuff)
+                {
+                    character.GetStatusEffectManager().RemoveStatusEffect(status);
+                }
+            }
             character.GetStatusEffectManager().AddStatusEffect(new Sanctified(2));
 
         }
