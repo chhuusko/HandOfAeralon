218dd188b84c0d278667edda94f873df82e42c64
MRbubbelgum <malte.gundhus@gmail.com>
2025-12-07 20:15:10 +0100

    VFX progress

--- Proj/Assets/Scripts/VFX/AbilityVFXSequence.cs
+++ Proj/Assets/Scripts/VFX/AbilityVFXSequence.cs
@@ -1,16 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
-public class AbilityVFXSequence : MonoBehaviour
+[CreateAssetMenu]
+public class AbilityVFXSequence : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private VFXPlayer castFX;
+    [SerializeField] private ProjectileVFXPlayer travelFX;   // projectile or null
+    [SerializeField] private VFXPlayer impactFX;
 
-    // Update is called once per frame
-    void Update()
+    public virtual IEnumerator RunSequence(VFXData data)
     {
-        
+        if (castFX != null)
+        {
+            Instantiate(castFX).Play(data.OriginPosition, data.Direction);
+            yield return new WaitForSeconds(data.CastingFXDuration);
+        }
+
+        if (travelFX != null)
+        {
+            var projectile = Instantiate(travelFX);
+            yield return projectile.PlayProjectile(data.OriginPosition,data.TargetPosition
+            );
+        }
+
+        if (impactFX != null)
+        {
+            Instantiate(impactFX).Play(data.TargetPosition, data.Direction);
+        }
     }
 }
