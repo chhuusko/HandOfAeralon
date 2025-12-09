3611116f48ee7b7bfca9830530a93a2356c9c306
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-12-09 11:25:15 +0100

    increasingShopCost

--- Proj/Assets/Scripts/UI/Shop.cs
+++ Proj/Assets/Scripts/UI/Shop.cs
@@ -50,7 +50,6 @@ public class Shop : MonoBehaviour
         _refreshText.text = "Refresh <color=Yellow>"+_refreshPrice+"</color><voffset=15><space=20><sprite name=\"UI_icon_59\">";
         _healText.text = "Heal Party (50%)\r\n<color=Yellow>"+ _healPrice+ "</color><voffset=15><space=20><sprite name=\"UI_icon_59\">";
         _instance = this;
-        Application.targetFrameRate = -1;
 
         _buyableItemInScene = new List<GameObject>();
         _partyPortraitInstances = new List<GameObject>();
@@ -61,7 +60,12 @@ public class Shop : MonoBehaviour
         LoadBuyCard();
         LoadBuyCharacter();
     }
-
+    public static System.Action onSellCard;
+    public void SoldCard() {
+        _removeCardPrice += _addedRemoveCardPrice;
+        _removeCardText.text = "Remove card <color=yellow>" + _removeCardPrice + "</color><voffset=15><space=25><sprite name=\"UI_icon_59\">";
+        onSellCard?.Invoke(); 
+    }
     public void LoadParty()
     {
         if (_partyPortraitInstances.Count > 0)
@@ -121,10 +125,6 @@ public class Shop : MonoBehaviour
         _deckTab.GetComponent<CardViewUI>().UpdateCards(GlobalGameManager.GetInstance().GetGameData().cardList);
         _deckTab.SetActive(true);
     }
-    public void SellCard()
-    {
-        
-    }
     public void Refresh()
     {
         if (CanAfford(_refreshPrice))
@@ -170,7 +170,7 @@ public class Shop : MonoBehaviour
         }
         LoadParty();
     }
-    public bool CanAfford(int cost)
+    public static bool CanAfford(int cost)
     {
         return GlobalGameManager.GetInstance().GetGameData().coins >= cost;
     }
