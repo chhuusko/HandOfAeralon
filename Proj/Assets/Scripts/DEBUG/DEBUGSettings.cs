ee926f606313b5e5b428c8438fa81d04b7b2ddbc
xWormx <42252397+xWormx@users.noreply.github.com>
2025-12-09 19:17:28 +0100

    EDIT - debugsettings

--- Proj/Assets/Scripts/DEBUG/DEBUGSettings.cs
+++ Proj/Assets/Scripts/DEBUG/DEBUGSettings.cs
@@ -19,10 +19,8 @@ public static class DebugLog
 
     private static void LoadSettings()
     {
-        #if UNITY_EDITOR
         if (_settings == null)
             _settings = Resources.Load<DEBUGSettings>("ScriptableObjects/DEBUGSettings");
-        #endif
     }
 
     // Standard Logs
