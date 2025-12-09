38bc8f8761a68a4bca8116893edd461f94b2d108
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-12-09 10:47:09 +0100

    ui price in code

--- Proj/Assets/Scripts/Card/Shop/SellableCardUI.cs
+++ Proj/Assets/Scripts/Card/Shop/SellableCardUI.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -13,12 +16,18 @@ public class SellableCardUI : MonoBehaviour, IPointerDownHandler, IPointerUpHand
     private float _sellTime = 2f;
     private float _timeHeld = 0;
     private Image _image;
+
     [SerializeField] Image _fillImage;
     [SerializeField] GameObject _soldText;
+    [SerializeField] TextMeshProUGUI _priceText;
+
+    private int _cost;
     private void Awake()
     {
+        _cost = Shop.GetInstance().GetRemoveCardPrice();
         _fillImage.fillAmount = 0;
         _image = GetComponent<Image>();
+        _priceText.text = "<color=Yellow>" + _cost + "</color><voffset=12><space=15><sprite name=\"UI_icon_59\">";
 
     }
     private void Update()
@@ -57,6 +66,6 @@ public class SellableCardUI : MonoBehaviour, IPointerDownHandler, IPointerUpHand
     private void Sell()
     {
         GlobalGameManager.GetInstance().GetGameData().cardList.Remove(_card);
-        Shop.GetInstance().ChangeCoins(-30);
+        Shop.GetInstance().ChangeCoins(-_cost);
     }
 }
