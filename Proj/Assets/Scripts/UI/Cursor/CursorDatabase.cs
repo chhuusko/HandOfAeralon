10ba47349e4094e3d03c7640279b078c3788d7ad
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-09 20:43:39 +0100

    Fixed some cursor size issues

--- Proj/Assets/Scripts/UI/Cursor/CursorDatabase.cs
+++ Proj/Assets/Scripts/UI/Cursor/CursorDatabase.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CursorDatabase", menuName = "Scriptable Objects/CursorDatabase")]
 public class CursorDatabase : ScriptableObject
 {
+    public bool useUICursor;
+    
     public Texture2D defaultCursor;
     public Texture2D dragCursor;
     public Texture2D hoverCursor;
