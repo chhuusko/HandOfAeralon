073d4c0b7428816cf4b659a7d606c9972a52a5f5
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-12-11 13:53:43 +0100

    CardRarityUI

--- Proj/Assets/Scripts/Card/Card.cs
+++ Proj/Assets/Scripts/Card/Card.cs
@@ -31,6 +31,13 @@ public class Card : ScriptableObject
     public List<InfoPanel> info;
     public List<CardTag> tags;
 
+    private List<Color> rarityColors = new List<Color>()
+    {
+        new Color(0.80f, 0.54f, 0.49f),
+        new Color(1f,1f,1f),
+        new Color(1.00f, 0.66f, 0.14f)
+    };
+
     private int tempCost;
     private bool isTempCost;
     private void Awake()
@@ -79,6 +86,10 @@ public class Card : ScriptableObject
         isTempCost = true;
         tempCost += changeInCost;
         if (tempCost < 0) { tempCost = 0; }
+    }
+    public Color GetRarityColor(int rarity)
+    {
+        return rarityColors[rarity];
     }
 }
 
