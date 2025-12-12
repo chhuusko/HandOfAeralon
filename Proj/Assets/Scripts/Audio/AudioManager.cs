a34a4fce385f502d2cb53ad9659855eebfc09c7e
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-12 17:11:04 +0100

    Added keeping track of current event instances

--- Proj/Assets/Scripts/Audio/AudioManager.cs
+++ Proj/Assets/Scripts/Audio/AudioManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using FMODUnity;
 using FMOD.Studio;
 using UnityEngine;
@@ -6,6 +7,9 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
+    
+    private List<EventInstance> events;
+    
     private void Awake()
     {
         if (Instance == null)
@@ -16,6 +20,8 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        
+        events = new List<EventInstance>();
     }
 
     public void PlayOneShot(EventReference sound, Vector3 position)
@@ -26,6 +32,22 @@ public class AudioManager : MonoBehaviour
     public EventInstance CreateInstance(EventReference eventReference)
     {
         EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        events.Add(eventInstance);
+        
         return eventInstance;
+    }
+
+    private void CleanUp()
+    {
+        foreach (EventInstance eventInstance in events)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
     }
 }
