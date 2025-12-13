9d3f577b5c9787cbb734bf9a17df35df1d3ce118
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-13 17:16:10 +0100

    Added combat log data class skeletons

---
+++ Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogData/CardLogData.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class CardLogData : CombatLogData
+{
+    
+}
--- Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogEntry.cs
+++ Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogEntry.cs
@@ -3,14 +3,5 @@ using UnityEngine;
 
 public class CombatLogEntry : MonoBehaviour
 {
-    public Ability Ability { get; private set; }
-    public CharacterData Source { get; private set; }
-    public CharacterData Target { get; private set; }
-
-    public CombatLogEntry(Ability ability, CharacterData source, CharacterData target)
-    {
-        Ability = ability;
-        Source = source;
-        Target = target;
-    }
+    
 }
