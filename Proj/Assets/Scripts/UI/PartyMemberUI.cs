79dc4bf55927105a28192834c4b4e6d4c951604c
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-12-13 13:22:39 +0100

    derivedhealth

--- Proj/Assets/Scripts/UI/PartyMemberUI.cs
+++ Proj/Assets/Scripts/UI/PartyMemberUI.cs
@@ -12,7 +12,7 @@ public class PartyMemberUI : MonoBehaviour
     public void SetUIElements(CharacterData characterData)
     {
         _image.sprite = characterData.ClassData.classImage;
-        _healthFill.fillAmount = 1f-((float)characterData.CurrentHealthPoints/ (float)characterData.BaseHealthPoints);
+        _healthFill.fillAmount = 1f-((float)characterData.CurrentHealthPoints/ (float)characterData.DerivedHealthPoints);
         _healthText.text = characterData.CurrentHealthPoints + "/" + characterData.BaseHealthPoints + "<voffset=12.5> <space=3> <sprite name=\"UI_icon_104\">";
     }
 }
