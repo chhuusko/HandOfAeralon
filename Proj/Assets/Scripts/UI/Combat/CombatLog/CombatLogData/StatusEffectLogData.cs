a772b80c6ba29ce040edfbaa23bd1a5e38154bb3
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-14 13:33:06 +0100

    StatusEffect log class skeletons

---
+++ Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogData/StatusEffectLogData.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class StatusEffectLogData : CombatLogData
+{
+    public StatusEffect StatusEffect;
+}
---
+++ Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogEntry/StatusEffectLogEntry.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class StatusEffectLogEntry : CombatLogEntry
+{
+    public override void Initialize(CombatLogData data)
+    {
+        throw new System.NotImplementedException();
+    }
+}
