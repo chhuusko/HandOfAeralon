3f12808c35330d3acc49fdf3750e2c11654b36cd
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-14 13:40:40 +0100

    Added serialized image and text fields

--- Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogEntry/CombatLogEntry.cs
+++ Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogEntry/CombatLogEntry.cs
@@ -1,7 +1,12 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class CombatLogEntry : MonoBehaviour
 {
+    [SerializeField] protected Image _image;
+    [SerializeField] protected TMP_Text _text;
+    
     public abstract void Initialize(CombatLogData data);
 }
