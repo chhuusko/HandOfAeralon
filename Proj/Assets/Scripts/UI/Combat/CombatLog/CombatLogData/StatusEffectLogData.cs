3a7266ed8f8fe9ff39095ce26a00c9eff950f664
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-15 11:32:24 +0100

    Build settings

--- Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogData/StatusEffectLogData.cs
+++ Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogData/StatusEffectLogData.cs
@@ -3,4 +3,6 @@ using UnityEngine;
 public class StatusEffectLogData : CombatLogData
 {
     public StatusEffect StatusEffect;
+    public Character Source;
+    public Character Target;
 }
--- Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogEntry/StatusEffectLogEntry.cs
+++ Proj/Assets/Scripts/UI/Combat/CombatLog/CombatLogEntry/StatusEffectLogEntry.cs
@@ -4,6 +4,6 @@ public class StatusEffectLogEntry : CombatLogEntry
 {
     public override void Initialize(CombatLogData data)
     {
-        throw new System.NotImplementedException();
+        
     }
 }
