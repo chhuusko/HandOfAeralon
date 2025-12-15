7c475b2d76b3cf245efb6b6d2e7094b4e0cbc69d
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-15 09:03:14 +0100

    Added remaining colors

--- Proj/Assets/Scripts/UI/ColorDatabase.cs
+++ Proj/Assets/Scripts/UI/ColorDatabase.cs
@@ -17,11 +17,29 @@ public class ColorDatabase : ScriptableObject
         }
     }
     
+    [Header("Characters")]
     public Color BarbarianColor;
     public Color BardColor;
     public Color RogueColor;
     public Color SorceressColor;
     public Color EnemyColor;
+
+    [Header("Abilities")] 
+    public Color AbilityColor;
+    public Color ElementalDamageColor;
+    public Color PhysicalDamageColor;
+    
+    [Header("Status Effects")]
+    public Color NonDamagingEffectColor;
+    public Color BurnColor;
+    public Color PoisonColor;
+
+    [Header("Cards")] 
+    public Color CardColor;
+    public Color ManaColor;
+    
+    [Header("Misc")]
+    public Color HealingColor;
     
     public Color GetCharacterColor(Character c)
     {
