f99aaa1c09f1b973554b88a1dfe8b3b84df2430f
MRbubbelgum <malte.gundhus@gmail.com>
2025-12-16 17:03:01 +0100

    Camera shake progress & Joppa lute smash bugfix

---
+++ Proj/Assets/Scripts/ManagerScripts/CameraShakeManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraShakeManager : MonoBehaviour
+{
+    public static CameraShakeManager _instance {  get; private set; }
+
+    private void Awake()
+    {
+        if(_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration) {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
