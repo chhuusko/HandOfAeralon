66f94e78fb26fb3bdae6bef4c7ac8410a19bf0e2
xWormx <42252397+xWormx@users.noreply.github.com>
2025-12-16 13:52:35 +0100

    EDIT - Save JSON now uses character.data.basedamage

--- Proj/Assets/Scripts/Combat/Character/CombatGridCharacterData.cs
+++ Proj/Assets/Scripts/Combat/Character/CombatGridCharacterData.cs
@@ -65,7 +65,8 @@ public class CombatGridCharacterData
 
         _baseHealthPoints      = characterScript.Data.BaseHealthPoints;
         _baseSpeed             = characterScript.GetBaseInitiative();
-        _baseDamage            = characterScript.GetBaseDamage();
+        //_baseDamage            = characterScript.GetBaseDamage();
+        _baseDamage            = characterScript.Data.BaseDamage;
         _baseMovementPoints    = characterScript.GetBaseMovementPoints();
 
         _currentHealthPoints   = characterScript.GetCurrentHealth();
