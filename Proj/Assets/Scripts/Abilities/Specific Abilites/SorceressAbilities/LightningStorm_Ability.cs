b821f1f3027763c512da947c80dcee38a88e4f6e
MRbubbelgum <malte.gundhus@gmail.com>
2025-12-16 11:24:03 +0100

    Update LightningStorm_Ability.cs

--- Proj/Assets/Scripts/Abilities/Specific Abilites/SorceressAbilities/LightningStorm_Ability.cs
+++ Proj/Assets/Scripts/Abilities/Specific Abilites/SorceressAbilities/LightningStorm_Ability.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Rendering.FilterWindow;
-using UnityEngine.Rendering.PostProcessing;
 
 [CreateAssetMenu(fileName = "LightningStorm_Ability", menuName = "Scriptable Objects/Abilities/Sorceress/Lightning Storm")]
 
