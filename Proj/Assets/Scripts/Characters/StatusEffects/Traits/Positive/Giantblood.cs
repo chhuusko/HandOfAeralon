b85879c64d5fe98a53de88b72d003dcfb6273fcd
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-17 06:01:47 +0100

    Fixed conduit of power damage calculation

--- Proj/Assets/Scripts/Characters/StatusEffects/Traits/Positive/Giantblood.cs
+++ Proj/Assets/Scripts/Characters/StatusEffects/Traits/Positive/Giantblood.cs
@@ -2,22 +2,6 @@ using UnityEngine;
 
 public class Giantblood : Trait
 {
-    private int _maxHealth;
-    
-    // public override void OnApply()
-    // {
-    //     var data = Data as DamageModifyingData;
-    //
-    //     if (!data)
-    //     {
-    //         return;
-    //     }
-    //     
-    //     _maxHealth = Character.GetMaxHealth();
-    //     
-    //     Character.SetDerivedHealthPoints(Mathf.RoundToInt(_maxHealth * data.DamageModifier));
-    // }
-
     public override void ModifyDerivedStats(ref float hpFactor, ref float damageFactor)
     {
         var data = Data as DamageModifyingData;
