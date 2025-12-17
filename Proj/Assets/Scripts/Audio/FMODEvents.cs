3ab005a529deda8fc358da8f4278e78439742c94
Joel Larsson Wendt <156666927+joellarssonwendt@users.noreply.github.com>
2025-12-17 11:13:03 +0100

    Alla klasser kopplade till sina separata footstep FMOD events

--- Proj/Assets/Scripts/Audio/FMODEvents.cs
+++ Proj/Assets/Scripts/Audio/FMODEvents.cs
@@ -15,7 +15,7 @@ public class FMODEvents : MonoBehaviour
     [SerializeField] private EventReference bardDeath;
     [SerializeField] private EventReference sorceressTakeDamage;
     [SerializeField] private EventReference sorceressDeath;
-    [SerializeField] private EventReference barbarianFootsteps;
+    [SerializeField] private EventReference barbarianFootsteps, bardFootsteps, rogueFootsteps, sorceressFootsteps;
     [SerializeField] private string damageParameter;
     
     public EventReference BarbarianTakeDamage => barbarianTakeDamage;
@@ -27,6 +27,10 @@ public class FMODEvents : MonoBehaviour
     public EventReference SorceressTakeDamage => sorceressTakeDamage;
     public EventReference SorceressDeath => sorceressDeath;
     public EventReference BarbarianFootsteps => barbarianFootsteps;
+    public EventReference BardFootsteps => bardFootsteps;
+    public EventReference RogueFootsteps => rogueFootsteps;
+    public EventReference SorceressFootsteps => sorceressFootsteps;
+
     public string DamageParameter => damageParameter;
 
     private void Awake()
