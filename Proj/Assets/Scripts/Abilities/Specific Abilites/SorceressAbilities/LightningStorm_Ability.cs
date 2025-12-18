543cc5ff188aaca7282fb55b3560d7572afd21f9
MRbubbelgum <malte.gundhus@gmail.com>
2025-12-18 12:28:43 +0100

    can move

--- Proj/Assets/Scripts/Abilities/Specific Abilites/SorceressAbilities/LightningStorm_Ability.cs
+++ Proj/Assets/Scripts/Abilities/Specific Abilites/SorceressAbilities/LightningStorm_Ability.cs
@@ -90,10 +90,10 @@ public class LightningStorm_Ability : RoundAOEAbility
         // Get base damage.
         int baseDamage = castingCharacter.Data.DerivedDamage;
 
-        int damage = (int)(baseDamage * _damageMultiplier);
+        int damage = Mathf.RoundToInt(baseDamage * _damageMultiplier);
 
-        damage = (int)castingCharacter.GetStatusEffectManager().ModifyOutgoingDamage(damage, this);
-        damage = (int)affectedCharacter.GetStatusEffectManager().ModifyIncomingDamage(damage, this);
+        damage = Mathf.RoundToInt(castingCharacter.GetStatusEffectManager().ModifyOutgoingDamage(damage, this));
+        damage = Mathf.RoundToInt(affectedCharacter.GetStatusEffectManager().ModifyIncomingDamage(damage, this));
         return damage;
     }
 
@@ -104,8 +104,8 @@ public class LightningStorm_Ability : RoundAOEAbility
 
     public override int GetDamage()
     {
-        int damage = (int)(GetCharacterCaster().Data.DerivedDamage * _damageMultiplier);
-        damage = (int)GetCharacterCaster().GetStatusEffectManager().ModifyOutgoingDamage(damage, this);
+        int damage = Mathf.RoundToInt(GetCharacterCaster().Data.DerivedDamage * _damageMultiplier);
+        damage = Mathf.RoundToInt(GetCharacterCaster().GetStatusEffectManager().ModifyOutgoingDamage(damage, this));
         return damage;
     }
 }
