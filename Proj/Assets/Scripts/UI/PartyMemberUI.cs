f9c26e2f206961fcb6887af11e69b0190a4b44d9
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2025-12-23 10:32:15 +0100

    partymember ui

--- Proj/Assets/Scripts/UI/PartyMemberUI.cs
+++ Proj/Assets/Scripts/UI/PartyMemberUI.cs
@@ -17,7 +17,7 @@ public class PartyMemberUI : MonoBehaviour, IPointerClickHandler
         _characterData = characterData;
         _image.sprite = characterData.ClassData.classImage;
         _healthFill.fillAmount = 1f-((float)characterData.CurrentHealthPoints/ (float)characterData.DerivedHealthPoints);
-        _healthText.text = characterData.CurrentHealthPoints + "/" + characterData.DerivedHealthPoints + "<voffset=12.5> <space=3> <sprite name=\"UI_icon_104\">";
+        _healthText.text = "Health " + characterData.CurrentHealthPoints + "/" + characterData.DerivedHealthPoints + "<voffset=12.5> <space=3> <sprite name=\"UI_icon_104\">";
         _sellText.text = "Sell " + _sellPrice + "<voffset=12.5> <space=3> <sprite name=\"UI_icon_59\">";
     }
     public void OnPointerClick(PointerEventData eventData)
