e4cbff556a85ac589d75223e5e0ef0d2d59ae2ff
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-26 11:56:54 +0100

    Added Resonant Recovery

--- Proj/Assets/Scripts/Characters/StatusEffects/Traits/Bard/ResonantRecovery.cs
+++ Proj/Assets/Scripts/Characters/StatusEffects/Traits/Bard/ResonantRecovery.cs
@@ -4,20 +4,37 @@ public class ResonantRecovery : Trait
 {
     public override void ModifyOutgoingDamage(ref float damage, Ability ability)
     {
+        if (ability is not ResonantBlastAOE)
+        {
+            return;
+        }
+        
+        var data = Data as DamageModifyingData;
+
+        if (!data)
+        {
+            return;
+        }
+        
+        damage *= data.DamageModifier;
+    }
+
+    public override void ModifyIncomingHeal(ref float heal, Ability ability)
+    {
+        if (ability is not ResonantBlastAOE)
+        {
+            return;
+        }
+        
         var data = Data as ResonantRecoveryData;
 
         if (!data)
         {
             return;
         }
+        
+        int healAmount = Mathf.RoundToInt(data.HealModifier * Character.GetMaxHealth());
 
-        if (ability is not ResonantBlastAOE)
-        {
-            return;
-        }
-        
-        damage *= data.DamageModifier;
-        
-        
+        Character.Heal(healAmount);
     }
 }
