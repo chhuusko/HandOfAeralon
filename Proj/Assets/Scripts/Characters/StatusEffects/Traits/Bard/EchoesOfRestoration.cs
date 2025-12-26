4d5ecbf21c0f793ad118d4606cf8e623356ce543
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-26 11:41:52 +0100

    Fixes to Echoes of Restoration

--- Proj/Assets/Scripts/Characters/StatusEffects/Traits/Bard/EchoesOfRestoration.cs
+++ Proj/Assets/Scripts/Characters/StatusEffects/Traits/Bard/EchoesOfRestoration.cs
@@ -3,18 +3,8 @@ using UnityEngine;
 public class EchoesOfRestoration : Trait
 {
     private float _healModifier = 1f;
+    private bool _shouldReset;
     
-    public override void ModifyOutgoingHeal(ref float heal, Ability ability)
-    {
-        if (_healModifier <= 1f)
-        {
-            return;
-        }
-        
-        heal *= _healModifier;
-        _healModifier = 1f;
-    }
-
     public override void OnAbilityUsed(AbilityExecutionData abilityData)
     {
         if (abilityData.Ability is not ResonantBlastAOE)
@@ -30,5 +20,29 @@ public class EchoesOfRestoration : Trait
         }
         
         _healModifier += data.Modifier;
+    }
+    
+    public override void ModifyOutgoingHeal(ref float heal, Ability ability)
+    {
+        if (ability is not SongOfRenewalAOE)
+        {
+            return;
+        }
+        
+        if (_healModifier <= 1f)
+        {
+            return;
+        }
+        
+        heal *= _healModifier;
+        _shouldReset = true;
+    }
+
+    public override void OnTurnEnd()
+    {
+        if (_shouldReset)
+        {
+            _healModifier = 1f;
+        }
     }
 }
