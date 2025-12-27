dcc3deaf65af0a817a79dae14abcb73739021d60
Joel Larsson Wendt <156666927+joellarssonwendt@users.noreply.github.com>
2025-12-27 01:59:50 +0100

    AI 3.2

--- Proj/Assets/Scripts/AI Controller/AI_Evaluator.cs
+++ Proj/Assets/Scripts/AI Controller/AI_Evaluator.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
-using static UnityEngine.GraphicsBuffer;
 
 public class AI_Evaluator : MonoBehaviour
 {
--- Proj/Assets/Scripts/AI Controller/EnemyAI.cs
+++ Proj/Assets/Scripts/AI Controller/EnemyAI.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEngine.GraphicsBuffer;
 
 public class EnemyAI : MonoBehaviour
 {
