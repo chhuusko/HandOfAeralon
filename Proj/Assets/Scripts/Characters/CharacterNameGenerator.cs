3dde181f40f5cf6e994065bc754b656a6a386d9b
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2025-12-30 19:06:59 +0100

    Removed unused code

--- Proj/Assets/Scripts/Characters/CharacterNameGenerator.cs
+++ Proj/Assets/Scripts/Characters/CharacterNameGenerator.cs
@@ -21,38 +21,6 @@ public class CharacterNameGenerator
             _allNamesPerClass[classData] = new List<string>(classData.availableNames);
         }
     }
-    
-    // public static string GenerateName(ClassData classData)
-    // {
-    //     if (!classData)
-    //     {
-    //         return null;
-    //     }
-    //
-    //     if (!_isInitialized)
-    //     {
-    //         Initialize();
-    //     }
-    //     
-    //     if (!_availableNamesPerClass.ContainsKey(classData) || _availableNamesPerClass[classData].Count == 0)
-    //     {
-    //         // Resets all names for this class if none are left.
-    //         _availableNamesPerClass[classData] = new List<string>(classData.availableNames);
-    //     }
-    //     
-    //     var names = _availableNamesPerClass[classData];
-    //     if (names.Count == 0)
-    //     {
-    //         Debug.LogError("No names available");
-    //         return "NoName";
-    //     }
-    //     
-    //     int index = UnityEngine.Random.Range(0, _availableNamesPerClass[classData].Count);
-    //     string name = names[index];
-    //     _availableNamesPerClass[classData].RemoveAt(index);
-    //     
-    //     return name;
-    // }
 
     public static string GenerateName(ClassData classData, HashSet<string> reservedNames)
     {
