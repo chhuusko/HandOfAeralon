11b9eb496c1b902c7de3b8f77ba26a327b11c807
Joel Larsson Wendt <156666927+joellarssonwendt@users.noreply.github.com>
2026-01-05 13:41:35 +0100

    tutorial progress

--- Proj/Assets/Scripts/Tutorial/Tutorial.cs
+++ Proj/Assets/Scripts/Tutorial/Tutorial.cs
@@ -1,4 +1,3 @@
-using TMPro;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour
@@ -18,9 +17,10 @@ public class Tutorial : MonoBehaviour
         }
     }
 
-    [SerializeField] private RectTransform _turnOrder;
+    [SerializeField] private RectTransform[] _popups;
 
     private Canvas _canvas;
+    private int _currentPopup = 0;
 
     void Start()
     {
@@ -30,5 +30,51 @@ public class Tutorial : MonoBehaviour
         {
             Debug.LogError("Tutorial.cs | Canvas not found!");
         }
+
+        HidePopups();
+    }
+
+    public void ShowPopup(int popup)
+    {
+        if (popup > 0 && popup < _popups.Length)
+        {
+            for (int i = 0; i < _popups.Length; i++)
+            {
+                _popups[i].gameObject.SetActive(false);
+            }
+
+            _popups[popup].gameObject.SetActive(true);
+            _currentPopup = popup;
+        }
+        else
+        {
+            Debug.LogError($"ShowPopup({popup}) INDEX OUT OF BOUNDS for {name}.");
+        }
+    }
+
+    public void HidePopups()
+    {
+        for (int i = 0; i < _popups.Length; i++)
+        {
+            _popups[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void NextPopup()
+    {
+        _currentPopup++;
+        ShowPopup(_currentPopup);
+    }
+
+    public void PreviousPopup()
+    {
+        _currentPopup--;
+        ShowPopup(_currentPopup);
+    }
+
+    public void Reset()
+    {
+        _currentPopup = 0;
+        HidePopups();
     }
 }
--- Proj/Assets/Scripts/Tutorial/TutorialPopup.cs
+++ Proj/Assets/Scripts/Tutorial/TutorialPopup.cs
@@ -1,6 +1,44 @@
+using TMPro;
 using UnityEngine;
 
 public class TutorialPopup : MonoBehaviour
 {
+    [SerializeField] private TMP_Text[] _pages;
+    private int _currentPage = 0;
 
+    public void ShowPage(int page)
+    {
+        if (page > 0 && page < _pages.Length)
+        {
+            for (int i = 0; i < _pages.Length; i++)
+            {
+                _pages[i].alpha = 0f;
+            }
+
+            _pages[page].alpha = 1f;
+            _currentPage = page;
+        }
+        else
+        {
+            Debug.LogError($"ShowPage({page}) INDEX OUT OF BOUNDS for {name}.");
+        }
+    }
+
+    public void NextPage()
+    {
+        _currentPage++;
+        ShowPage(_currentPage);
+    }
+
+    public void PreviousPage()
+    {
+        _currentPage--;
+        ShowPage(_currentPage);
+    }
+
+    public void Reset()
+    {
+        _currentPage = 0;
+        ShowPage(_currentPage);
+    }
 }
