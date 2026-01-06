463ee3fb3ad77b75ebefb9a1cde85a127a2d32f3
Joel Larsson Wendt <156666927+joellarssonwendt@users.noreply.github.com>
2026-01-06 01:36:21 +0100

    Tutorial 2 / 999

--- Proj/Assets/Scripts/Tutorial/Tutorial.cs
+++ Proj/Assets/Scripts/Tutorial/Tutorial.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
@@ -17,7 +19,8 @@ public class Tutorial : MonoBehaviour
         }
     }
 
-    [SerializeField] private RectTransform[] _popups;
+    [SerializeField] private TutorialPopup[] _popups;
+    [SerializeField] private Image _blocker;
 
     private Canvas _canvas;
     private int _currentPopup = 0;
@@ -32,23 +35,31 @@ public class Tutorial : MonoBehaviour
         }
 
         HidePopups();
+        
+        if (_currentPopup == 0)
+        {
+            StartCoroutine(FirstPopup());
+        }
     }
 
     public void ShowPopup(int popup)
     {
-        if (popup > 0 && popup < _popups.Length)
+        if (popup >= 0 && popup < _popups.Length)
         {
             for (int i = 0; i < _popups.Length; i++)
             {
                 _popups[i].gameObject.SetActive(false);
             }
 
+            _blocker.gameObject.SetActive(true);
+            Tooltipper._instance.HideTooltip();
             _popups[popup].gameObject.SetActive(true);
             _currentPopup = popup;
         }
         else
         {
             Debug.LogError($"ShowPopup({popup}) INDEX OUT OF BOUNDS for {name}.");
+            HidePopups();
         }
     }
 
@@ -56,8 +67,11 @@ public class Tutorial : MonoBehaviour
     {
         for (int i = 0; i < _popups.Length; i++)
         {
+            _popups[i].Reset();
             _popups[i].gameObject.SetActive(false);
         }
+
+        _blocker.gameObject.SetActive(false);
     }
 
     public void NextPopup()
@@ -77,4 +91,21 @@ public class Tutorial : MonoBehaviour
         _currentPopup = 0;
         HidePopups();
     }
+
+    private IEnumerator FirstPopup()
+    {
+        float maxTime = 8f; // Lika lång tid som intro-cinematic tar
+        float startTime = Time.time;
+
+        while (Time.time - startTime < maxTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                break;
+
+            yield return null;
+        }
+
+        ShowPopup(0);
+    }
+
 }
