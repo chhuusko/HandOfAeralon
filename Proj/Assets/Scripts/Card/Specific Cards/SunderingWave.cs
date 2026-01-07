f9f6318283e4a897da2387a1d6feb3df38f57601
AlexO04 <144449986+AlexO04@users.noreply.github.com>
2026-01-07 22:42:13 +0100

    Sundering wave fix?

--- Proj/Assets/Scripts/Card/Specific Cards/SunderingWave.cs
+++ Proj/Assets/Scripts/Card/Specific Cards/SunderingWave.cs
@@ -35,7 +35,7 @@ public class SunderingWave : Card
             {
                 int damage = 60;
                 damage = Mathf.RoundToInt(character.GetStatusEffectManager().ModifyIncomingDamage(damage, null));
-                character.TakeDamage(-damage);
+                character.PreviewHealthChange(-damage);
             }
             else
             {
