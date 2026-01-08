8bebed8e96962d23876fbca777204ae72b076155
Joppa0 <114592889+CardiganCarl@users.noreply.github.com>
2026-01-08 11:24:44 +0100

    Moved sanctified behavior to class

--- Proj/Assets/Scripts/Characters/StatusEffects/Buffs/Sanctified.cs
+++ Proj/Assets/Scripts/Characters/StatusEffects/Buffs/Sanctified.cs
@@ -6,7 +6,13 @@ public class Sanctified : StatusEffect
     public Sanctified(int duration) : base(duration)
     {
     }
-    
+
+    public override bool BeforeStatusEffectApplied(Character caster, Character target, StatusEffect statusEffect)
+    {
+        // Sanctified disallows receiving debuffs.
+        return statusEffect.Data.Type is not StatusEffectType.Debuff;
+    }
+
     public override void ModifyIncomingDamage(ref float damage, Ability ability)
     {
         damage = 0f;
