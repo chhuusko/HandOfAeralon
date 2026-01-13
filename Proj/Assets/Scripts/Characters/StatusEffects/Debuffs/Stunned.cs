b329f7e7ef80c7dfdba334cd5783f5a46a1a3ee5
xWormx <42252397+xWormx@users.noreply.github.com>
2026-01-13 14:58:00 +0100

    ADD - Stunned VFX to Characters

--- Proj/Assets/Scripts/Characters/StatusEffects/Debuffs/Stunned.cs
+++ Proj/Assets/Scripts/Characters/StatusEffects/Debuffs/Stunned.cs
@@ -9,10 +9,12 @@ public class Stunned : StatusEffect
     public override void OnApply()
     {
         Character.IsStunned = true;
+        Character.SpawnStunnedEffect();
     }
 
     public override void OnExpire()
     {
         Character.IsStunned = false;
+        Character.DestroyStunnedEffect();
     }
 }
