aae6ba2a60e3ee0734ad2c87ba4a668670076e6a
xWormx <42252397+xWormx@users.noreply.github.com>
2026-01-16 10:59:00 +0100

    EDIT - Fixed overhead sphere position match character scale

--- Proj/Assets/Scripts/Combat/CombatGrid.cs
+++ Proj/Assets/Scripts/Combat/CombatGrid.cs
@@ -443,10 +443,20 @@ public class CombatGrid : MonoBehaviour
         GameObject characterPrefab = _characterPrefabLibrary.GetPrefab(characterClass, faction);
         GameObject characterObject = Object.Instantiate(characterPrefab, instancePos, rotation);
 
-        
-        float randScale = Random.Range(1f, 1.6f);
-        Vector3 scale = new Vector3(randScale, randScale, randScale);
-        characterObject.transform.localScale = scale;
+        if(LevelManager.GetInstance().IsLastLevel())
+        {
+            float randScale = Random.Range(1.6f, 1.8f);
+            Vector3 scale = new Vector3(randScale, randScale, randScale);
+            characterObject.transform.localScale = scale;
+        }
+        else
+        {
+            float randScale = Random.Range(1f, 1.5f);
+            Vector3 scale = new Vector3(randScale, randScale, randScale);
+            characterObject.transform.localScale = scale;
+
+        }
+
         Character characterScript = characterObject.GetComponent<Character>();
 
         // Initialize CharacterData directly from JSON
