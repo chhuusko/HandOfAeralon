4200eb3752786e3e7c43ed1ee078af4b1d32c28d
Joel Larsson Wendt <156666927+joellarssonwendt@users.noreply.github.com>
2026-01-16 15:27:58 +0100

    Update EnemyAI.cs

--- Proj/Assets/Scripts/AI Controller/EnemyAI.cs
+++ Proj/Assets/Scripts/AI Controller/EnemyAI.cs
@@ -553,7 +553,7 @@ public class EnemyAI : MonoBehaviour
             // Bard
             case "InspiringAnthem_Ability":
                 {
-                    _turnEndWaitTimeDynamic = 4f;
+                    _turnEndWaitTimeDynamic = 4.3f;
 
                     int hitCount = 0;
                     List<CombatGridTile> aoe = DiamondPattern(target, 2);
